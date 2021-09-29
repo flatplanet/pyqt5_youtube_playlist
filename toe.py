22dff2b7f6b7b79dacfedcb17a8db643b0e0cd35
John Elder <johne4196@gmail.com>
2021-09-29 08:48:09 -0700

    tic tac toe logic

--- toe.py
+++ toe.py
@@ -41,6 +41,68 @@ class UI(QMainWindow):
 		# Show The App
 		self.show()
 
+	def checkWin(self):
+		# Across
+		if self.button1.text() != "" and self.button1.text() == self.button4.text() and self.button1.text() == self.button7.text():
+			self.win(self.button1, self.button4, self.button7)
+
+		if self.button2.text() != "" and self.button2.text() == self.button5.text() and self.button2.text() == self.button8.text():
+			self.win(self.button2, self.button5, self.button8)
+
+		if self.button3.text() != "" and self.button3.text() == self.button6.text() and self.button3.text() == self.button9.text():
+			self.win(self.button3, self.button6, self.button9)
+
+		# Down
+		if self.button1.text() != "" and self.button1.text() == self.button2.text() and self.button1.text() == self.button3.text():
+			self.win(self.button1, self.button2, self.button3)
+
+		if self.button4.text() != "" and self.button4.text() == self.button5.text() and self.button4.text() == self.button6.text():
+			self.win(self.button4, self.button5, self.button6)
+
+		if self.button7.text() != "" and self.button7.text() == self.button8.text() and self.button7.text() == self.button9.text():
+			self.win(self.button7, self.button8, self.button9)
+
+		# Diagonal
+		if self.button1.text() != "" and self.button1.text() == self.button5.text() and self.button1.text() == self.button9.text():
+			self.win(self.button1, self.button5, self.button9)
+
+		if self.button3.text() != "" and self.button3.text() == self.button5.text() and self.button3.text() == self.button7.text():
+			self.win(self.button3, self.button5, self.button7)
+
+
+
+
+
+	def win(self, a,b,c):
+		# Change the button colors to red
+		a.setStyleSheet('QPushButton {color: red;}')
+		b.setStyleSheet('QPushButton {color: red;}')
+		c.setStyleSheet('QPushButton {color: red;}')
+
+		# Add Winner Label
+		self.label.setText(f"{a.text()} Wins!")
+
+		# Disable The Board
+		self.disable()
+
+	# Disable The Board
+	def disable(self):
+		# Create a List of all our buttons
+		button_list = [
+		self.button1,
+		self.button2,
+		self.button3,
+		self.button4,
+		self.button5,
+		self.button6,
+		self.button7,
+		self.button8,
+		self.button9,]
+
+		# Reset The Buttons
+		for b in button_list:
+			b.setEnabled(False)
+
 	# Click The Buttons
 	def clicker(self, b):
 		if self.counter % 2 == 0:
@@ -56,6 +118,8 @@ class UI(QMainWindow):
 		# Increment The Counter
 		self.counter += 1
 
+		# Check if won
+		self.checkWin()
 
 	# Start Over
 	def reset(self):
@@ -75,12 +139,15 @@ class UI(QMainWindow):
 		for b in button_list:
 			b.setText("")
 			b.setEnabled(True)
+			# Reset The Button Colors
+			b.setStyleSheet('QPushButton {color: #797979;}')
 
 		# Reset The Label
 		self.label.setText("X Goes First!")
 
 		# Reset The Counter
 		self.counter = 0
+
 
 
 	
